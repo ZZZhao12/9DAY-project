5427050913b7278460fed00654177bf6d4951675
ZZZhao12 <13285519617@163.com>
2023-11-11 19:12:22 +0800

    测试代码

---
+++ text 11.11/test.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+int main()
+{
+	//printf("hehe\n");
+	int a = 0;
+	scanf("%d", &a);
+	printf("%d", a);
+	return 0;
+}
+
+//newc++filc.cpp
+//scanf
+//strcpy
+//strcat
